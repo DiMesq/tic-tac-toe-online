748859af01772340b6b69202f34168c0ab160ac0
DiMesq <d.mesquita285@gmail.com>
2016-04-17 10:36:47 +0100

    Adds the predefined messages for the GameManager.

---
+++ GameManagerMessages.py
@@ -0,0 +1,17 @@
+# errors
+INVALID_COMMAND = "Invalid command. Please type again."
+INVALID_USERNAME = "Error: invalid username. Please try a different name."
+USER_ALREADY_REGISTERED = "Error: already registered user. Changing names" + \
+  " is not possible"
+USER_NOT_REGISTERED = "Error: user not registered."
+USER_OCCUPIED = "Error: user occupied."
+
+# responses
+USER_REGISTER = "REG"
+LIST_USERS = "LST"
+USER_INVITE = "INV"
+USER_ACCEPTED = "ACP"
+
+
+
+
