e9f602f41ac63b15a64f36f3c447d31ad0621f9c
DiMesq <d.mesquita285@gmail.com>
2016-04-19 09:38:06 +0100

    Fix: fix game manager to correct version.

--- tic-tac-toe-server/src/server/GameManager.py
+++ tic-tac-toe-server/src/server/GameManager.py
@@ -57,7 +57,7 @@ class GameManager:
         returns [msg, send_addr]
           msg: the msg to be sent
           send_addr: to whom the message should be sent. If error, to this caller
-            If no error, to the invited player'''
+            If no error, to the invited player''' 
 
     # check if user is registered
     if addr not in self.clients:
@@ -70,6 +70,9 @@ class GameManager:
     # check if user is occupied
     if inviting_state[0]: return GameManagerMessages.USER_OCCUPIED, addr
 
+    # check if user is not inviting himself
+    if inviting == invited: return GameManagerMessages.INVALID_USERNAME, addr
+    
     # check if player invited exists and is not occupied
     if invited not in self.addrs:
       return GameManagerMessages.OTHER_USER_NOT_REGISTERED, addr
@@ -84,9 +87,9 @@ class GameManager:
     inviting_state[3] = invited
 
     # set invited user to occupied and as needing to respond
-    inviting_state[0] = inviting_state[2] = True
-    inviting_state[1] = False
-    inviting_state[3] = inviting
+    invited_state[0] = invited_state[2] = True
+    invited_state[1] = False
+    invited_state[3] = inviting
 
     #return send message to invited player
     return GameManagerMessages.USER_INVITE + " " + inviting, self.addrs[invited]
@@ -106,29 +109,28 @@ class GameManager:
     if addr not in self.clients:
       return GameManagerMessages.USER_NOT_REGISTERED, addr
 
-    # check if accepted has the correct format
-    if accepted in ('True', 'False'):
-      accepted_bool = True if accepted == 'True' else False
-    else:
-        return GameManagerMessages.INVALID_COMMAND, addr
-
     caller = self.clients[addr]
     caller_state = self.games[caller]
 
     # check if caller is in position to accept a request
-    #if not(caller_state[0] and not caller_state[1] and caller_state[2]):
-     # return GameManagerMessages.USER_CANT_ACCEPT, addr
+    if not(caller_state[0] and not caller_state[1] and caller_state[2]):
+      return GameManagerMessages.USER_CANT_ACCEPT, addr
+
+    # check if accepted has the correct format
+    if accepted in ('True', 'False'): 
+      accepted_bool = True if accepted == 'True' else False
+    else: return GameManagerMessages.INVALID_COMMAND, addr
 
     # check if refered player matches the player that made the initial invite
-    #if (caller_state[3] != other_player): return GameManagerMessages.REQUEST_PLAYER_MISMATCH, addr
+    if (caller_state[3] != other_player): return GameManagerMessages.REQUEST_PLAYER_MISMATCH, addr
 
     other_player_state = self.games[other_player]
 
     # put players in the correct state for game if user accepted
     if accepted_bool:
-      caller_state[1] = True
+      caller_state[1] = True 
       caller_state[2] = False
-    #  other_player[2] = True
+      other_player_state[2] = True 
 
     # otherwise put players in begin state
     else:
@@ -145,27 +147,26 @@ class GameManager:
         is_finito: string, 'true' if play ends the game and 'false' if not
 
         returns [msg, send_to] '''
-
+    
     # check if caller is registered
     if addr not in self.clients:
       return GameManagerMessages.USER_NOT_REGISTERED, addr
 
     # check if accepted has the correct format
-    if is_finito in ('True', 'False'):
+    if is_finito in ('True', 'False'): 
       is_finito_bool = True if is_finito == 'True' else False
-    else:
-        return GameManagerMessages.INVALID_COMMAND, addr
+    else: return GameManagerMessages.INVALID_COMMAND, addr
 
     player = self.clients[addr]
     player_state = self.games[player]
 
     # check if player is in position to make a play
-    #if not(player_state[0] and player_state[1] and player_state[2]):
-    #  return GameManagerMessages.USER_CANT_PLAY, addr
+    if not(player_state[0] and player_state[1] and player_state[2]):
+      return GameManagerMessages.USER_CANT_PLAY, addr
 
     # check if the other player is specified correctly
-    #if other_player != player_state[3]:
-    #  return GameManagerMessages.REQUEST_PLAYER_MISMATCH, addr
+    if other_player != player_state[3]:
+      return GameManagerMessages.REQUEST_PLAYER_MISMATCH, addr
 
     other_player_state = self.games[other_player]
 
@@ -198,3 +199,6 @@ class GameManager:
   def __default(self, state1):
     state1[0] = state1[1] = state1[2] = False
     state1[3] = None
+
+
+      
